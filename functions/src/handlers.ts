5f4670e695af1437a34d5129e1157fcf00dea134
Dudi <dudizimber@hotmail.com>
2020-06-18 13:08:19 +0300

    tslint

--- functions/src/handlers.ts
+++ functions/src/handlers.ts
@@ -5,16 +5,16 @@ const getAccessToken = (req: any, res: any) => {
 
     console.log('Getting Access Token', req.query);
 
-    var AccessToken = require('twilio').jwt.AccessToken;
-    var VideoGrant = AccessToken.VideoGrant;
+    const AccessToken = require('twilio').jwt.AccessToken;
+    const VideoGrant = AccessToken.VideoGrant;
 
     // Substitute your Twilio AccountSid and ApiKey details
-    var ACCOUNT_SID = 'ACbee50e6fc385c44262b929dc428457ff'
-    var API_KEY_SID = functions.config().twilio.sid;
-    var API_KEY_SECRET = functions.config().twilio.secret;
+    const ACCOUNT_SID = functions.config().twilio.account;
+    const API_KEY_SID = functions.config().twilio.sid;
+    const API_KEY_SECRET = functions.config().twilio.secret;
 
     // Create an Access Token
-    var accessToken = new AccessToken(
+    const accessToken = new AccessToken(
         ACCOUNT_SID,
         API_KEY_SID,
         API_KEY_SECRET
@@ -29,7 +29,7 @@ const getAccessToken = (req: any, res: any) => {
     accessToken.addGrant(grant);
 
     // Serialize the token as a JWT
-    var jwt = accessToken.toJwt();
+    const jwt = accessToken.toJwt();
     console.log(jwt);
 
     res.send(jwt);
